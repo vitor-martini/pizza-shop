5a8a43fe7f37666711dcb4db7295b3723abd0ac4
Vitor Martini <vitor-martini@outlook.com>
2024-09-26 22:16:46 -0300

    feat: sign in integration

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,4 +1,5 @@
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
@@ -8,6 +9,7 @@ import { z } from 'zod'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+import { signIn } from '@/src/sign-in'
 
 const signInForm = z.object({
   email: z.string().email('Informe um e-mail válido.'),
@@ -24,15 +26,22 @@ export function SignIn() {
     resolver: zodResolver(signInForm),
   })
 
+  const { mutateAsync: authenticate } = useMutation({
+    mutationFn: signIn,
+  })
+
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 2000))
-    toast.success('Enviamos um link de autenticação para seu e-mail!', {
-      action: {
-        label: 'Reenviar',
-        onClick: () => handleSignIn(data),
-      },
-    })
+    try {
+      await authenticate({ email: data.email })
+      toast.success('Enviamos um link de autenticação para seu e-mail!', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
+    } catch {
+      toast.error('Credenciais inválidas')
+    }
   }
 
   return (
