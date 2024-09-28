b3a72d84a04cfd7e2507fb49adc89409dd3c51a8
Vitor Martini <vitor-martini@outlook.com>
2024-09-28 18:23:50 -0300

    feat: filter orders

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -13,19 +13,29 @@ import {
   TableRow,
 } from '@/components/ui/table'
 
-import { OrderFilters } from './order-table-filters'
+import { OrderTableFilters } from './order-table-filters'
 import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
   const [searchParams, setSearchParams] = useSearchParams()
+  const orderId = searchParams.get('orderId')
+  const customerName = searchParams.get('customerName')
+  const status = searchParams.get('status')
+
   const pageIndex = z.coerce
     .number()
     .transform((page) => page - 1)
     .parse(searchParams.get('page') ?? '1')
 
   const { data: result } = useQuery({
-    queryKey: ['orders', pageIndex],
-    queryFn: () => getOrders({ pageIndex }),
+    queryKey: ['orders', pageIndex, orderId, customerName, status],
+    queryFn: () =>
+      getOrders({
+        pageIndex,
+        orderId,
+        customerName,
+        status: status === 'all' ? null : status,
+      }),
   })
 
   function handlePaginate(pageIndex: number) {
@@ -42,7 +52,7 @@ export function Orders() {
       <div className="flex flex-col gap-4">
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
         <div className="space-y-2.5">
-          <OrderFilters />
+          <OrderTableFilters />
           <div className="rounded-md border">
             <Table>
               <TableHeader>
