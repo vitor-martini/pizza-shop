78912e4c22fbfbff25df32ab5650fe839322d925
Vitor Martini <vitor-martini@outlook.com>
2024-10-04 23:16:25 -0300

    fix: change restaurant name

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,5 +1,4 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { DialogClose } from '@radix-ui/react-dialog'
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
 import { toast } from 'sonner'
@@ -13,6 +12,7 @@ import { updateProfile } from '@/api/update-profile'
 
 import { Button } from './ui/button'
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -32,23 +32,33 @@ type StoreProfileSchema = z.infer<typeof storeProfileSchema>
 
 export function StoreProfileDialog() {
   const queryClient = useQueryClient()
-  const { data: managedRestaurant } = useQuery({
+
+  const { data: storeProfile, isLoading: isLoadingStoreProfile } = useQuery({
     queryKey: ['managed-restaurant'],
     queryFn: getManagedRestaurant,
     staleTime: Infinity,
   })
 
-  function updateManagedRestaurantCache({
-    name,
-    description,
-  }: StoreProfileSchema) {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<StoreProfileSchema>({
+    resolver: zodResolver(storeProfileSchema),
+    values: {
+      name: storeProfile?.name ?? '',
+      description: storeProfile?.description ?? '',
+    },
+  })
+
+  function updateProfileDataOnCache({ name, description }: StoreProfileSchema) {
     const cached = queryClient.getQueryData<GetManagedRestaurantResponse>([
-      'managed-restaurent',
+      'managed-restaurant',
     ])
 
     if (cached) {
       queryClient.setQueryData<GetManagedRestaurantResponse>(
-        ['managed-restaurent'],
+        ['managed-restaurant'],
         {
           ...cached,
           name,
@@ -62,78 +72,82 @@ export function StoreProfileDialog() {
 
   const { mutateAsync: updateProfileFn } = useMutation({
     mutationFn: updateProfile,
-    onMutate({ name, description }) {
-      const { cached } = updateManagedRestaurantCache({ name, description })
+    onMutate: ({ name, description }) => {
+      const { cached } = updateProfileDataOnCache({
+        name,
+        description,
+      })
+
       return { previousProfile: cached }
     },
-    onError(error, __, context) {
-      console.log(error)
+    onError(_, __, context) {
       if (context?.previousProfile) {
-        updateManagedRestaurantCache(context.previousProfile)
+        updateProfileDataOnCache(context.previousProfile)
       }
     },
   })
 
-  const {
-    register,
-    handleSubmit,
-    formState: { isSubmitting },
-  } = useForm<StoreProfileSchema>({
-    resolver: zodResolver(storeProfileSchema),
-    values: {
-      name: managedRestaurant?.name ?? '',
-      description: managedRestaurant?.description ?? '',
-    },
-  })
-
-  async function handleUpdateProfile(data: StoreProfileSchema) {
+  async function handleUpdateProfile({
+    name,
+    description,
+  }: StoreProfileSchema) {
     try {
       await updateProfileFn({
-        name: data.name,
-        description: data.description,
+        name,
+        description,
       })
+
       toast.success('Perfil atualizado com sucesso!')
-    } catch (e) {
-      console.log(e)
-      toast.error('Erro ao atualizar o perfil')
+    } catch {
+      toast.error('Falha ao atualizar o perfil, tente novamente!')
     }
   }
 
   return (
-    <DialogContent>
+    <DialogContent className="sm:max-w-[520px]">
       <DialogHeader>
         <DialogTitle>Perfil da loja</DialogTitle>
         <DialogDescription>
-          Atualize as informações do seu estabelecimento
+          Atualize as informações do seu estabelecimento visíveis aos seus
+          clientes.
         </DialogDescription>
       </DialogHeader>
-
       <form onSubmit={handleSubmit(handleUpdateProfile)}>
-        <div className="space-y-4 py-4">
+        <div className="grid gap-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
-            <Label className="text-right" htmlFor="name">
+            <Label htmlFor="name" className="text-right">
               Nome
             </Label>
-            <Input className="col-span-3" id="name" {...register('name')} />
+            <Input
+              id="name"
+              className="col-span-3"
+              disabled={isLoadingStoreProfile}
+              {...register('name')}
+            />
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label className="text-right" htmlFor="description">
+          <div className="grid grid-cols-4 items-baseline gap-4">
+            <Label htmlFor="description" className="text-right">
               Descrição
             </Label>
             <Textarea
-              className="col-span-3"
               id="description"
+              className="col-span-3 min-h-[100px]"
+              disabled={isLoadingStoreProfile}
               {...register('description')}
             />
           </div>
         </div>
         <DialogFooter>
           <DialogClose asChild>
-            <Button variant="destructive" type="button">
+            <Button variant="ghost" type="button">
               Cancelar
             </Button>
           </DialogClose>
-          <Button variant="success" type="submit" disabled={isSubmitting}>
+          <Button
+            type="submit"
+            variant="success"
+            disabled={isLoadingStoreProfile || isSubmitting}
+          >
             Salvar
           </Button>
         </DialogFooter>
